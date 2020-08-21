ccda9297919fe03db657f83cf86d36b96bd17ff3
woodyvalentino <woodyvalentinomathias123@gmail.com>
2020-08-21 21:19:24 +0530

    chainbird

--- sketch.js
+++ sketch.js
@@ -1,11 +1,12 @@
 const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
 
 var engine, world;
 var box1, pig1;
 var backgroundImg,platform;
-
+var chain;
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
 }
@@ -33,9 +34,9 @@ function setup(){
     box5 = new Box(810,160,70,70);
     log4 = new Log(760,120,150, PI/7);
     log5 = new Log(870,120,150, -PI/7);
-
+    log6 = new Log(230,180,80,PI/2);
     bird = new Bird(100,100);
-
+    chain = new Chain(bird.body,log6.body)
 }
 
 function draw(){
@@ -61,4 +62,7 @@ function draw(){
 
     bird.display();
     platform.display();
+
+    chain.display();
+    log6.display();
 }
